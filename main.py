601db862d660f4dcf6bd3ac673d49e114daf2e70
Abhikkumar619 <abisheky194@gmail.com>
2024-06-28 06:56:55 +0545

    model training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.Customer_segementation.pipeline.stage_1_data_ingestion import DataIngestionPipeline
 from src.Customer_segementation.pipeline.stage_2_data_validation import DataValidationPipeline
 from src.Customer_segementation.pipeline.stage_3_data_transformation import DataTransformationPipeline
+from src.Customer_segementation.pipeline.stage_4_model_training import ModelTraningPipeline
 from src.Customer_segementation.logger import logger
 """ 
 stage_name="DataIngestion"
@@ -26,3 +27,12 @@ if __name__ == "__main__":
     obj=DataTransformationPipeline()
     obj.main()
     logger.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {stage_name} Completed >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+stage_name="Model_Training"
+
+if __name__ =="__main__": 
+    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {stage_name} started>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    obj=ModelTraningPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {stage_name} Completed >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
