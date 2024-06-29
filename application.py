9b02b4964530e486af1c9e0313555018d928d20b
Abhikkumar619 <abisheky194@gmail.com>
2024-06-29 06:44:46 +0545

    app completed

--- application.py
+++ application.py
@@ -23,6 +23,12 @@ def predict_datapoint():
             Spending_Score=request.form.get("Spending_Score"),
             Family_Size=int(request.form.get("Family_Size"))
         )
+
+        spending_scor=data.Spending_Score
+        family_length=data.Family_Size
+        designation=data.Profession
+
+        logger.info(f"Spending score: {spending_scor}, family_size is {family_length}, designation: {designation}")
     
         final_Data=data.get_data_as_dataframe()
         logger.info(f"Data from form\n: {final_Data}")
@@ -30,9 +36,21 @@ def predict_datapoint():
         predict_obj=PrediPipeline()
         final_pred=predict_obj.predict(user_dataframe=final_Data)
         logger.info(f"Prediction of model: {final_pred[0]}")
-        result=final_pred[0]
+        category=final_pred[0]
 
-        return render_template("result.html",final_result=result)
+
+        if category== 0:
+            result="A"
+            return render_template("result.html",final_result=f"{result}, having spending score :{spending_scor}, Family_size: {family_length} and degination: {designation}")
+        elif category == 1: 
+            result="D"    
+            return render_template("result.html",final_result=f"{result}, having spending score :{spending_scor}, Family_size: {family_length} and degination: {designation}")
+        elif category==2: 
+            result="B"
+            return render_template("result.html",final_result=f"{result}, having spending score :{spending_scor}, Family_size: {family_length} and degination: {designation}")
+        else:
+            result="C"
+            return render_template("result.html",final_result=f"{result}, having spending score :{spending_scor}, Family_size: {family_length} and degination: {designation}")   
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
